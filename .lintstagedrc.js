6f3766ffc4a8227449e6805fc3e90483c1ad3a1f
Chris Evans <chris@ceva24.dev>
2023-07-08 00:39:22 +0100

    style: update formatting of all files to match prettier rules

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
     "*.(js|jsx|ts|tsx|md|json|yml)": "prettier --write",
-    "src/**/*.(ts|tsx|noop)": () => "tsc",
+    "src/**/*.(ts|tsx|noop)": () => "tsc --noEmit",
 };
--- lint-staged.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "*.(js|jsx|ts|tsx|md|json|yml)": "prettier --write",
-    "src/**/*.(ts|tsx|noop)": () => "tsc",
-};
