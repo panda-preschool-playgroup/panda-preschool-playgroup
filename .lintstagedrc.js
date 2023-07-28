5566930abab52445d31e68a4ba660a512e146f28
Chris Evans <chris@ceva24.dev>
2023-07-28 17:54:39 +0100

    chore: update lint-staged to run cypress typescript checks

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,6 @@ module.exports = {
     "*.(json|md|yml)": "prettier --write",
     "*.(js|jsx|ts|tsx)": ["eslint --fix", "prettier --write"],
     "*.(js|jsx|ts|tsx|noop)": () => "jest --onlyChanged --bail",
-    "*.(ts|tsx)": () => "tsc",
+    "src/**/*.(ts|tsx)": () => "tsc",
+    "cypress/**/*": () => "tsc --project cypress/tsconfig.json",
 };
