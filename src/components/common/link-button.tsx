f7e0e4ee8d3d04f21ad1f9f666d5bbdc4b481457
Chris Evans <chris@ceva24.dev>
2023-07-31 22:45:04 +0100

    feat: update jumbotron button style

--- src/components/common/link-button.tsx
+++ src/components/common/link-button.tsx
@@ -3,25 +3,17 @@ import { Link } from "gatsby";
 interface LinkButtonProps {
     text: string;
     href: string;
-    style: string;
 }
 
-type ColoredLinkButtonProps = Pick<LinkButtonProps, "text" | "href">;
-
-const PrimaryLinkButton = ({ text, href }: ColoredLinkButtonProps) => {
-    return <LinkButton text={text} href={href} style="bg-cyan hover:bg-cyan-dark" />;
-};
-
-const SecondaryLinkButton = ({ text, href }: ColoredLinkButtonProps) => {
-    return <LinkButton text={text} href={href} style="bg-blue hover:bg-blue-dark " />;
-};
-
-const LinkButton = ({ text, href, style }: LinkButtonProps) => {
+const LinkButton = ({ text, href }: LinkButtonProps) => {
     return (
-        <Link to={href} className={`${style} text-gray-100 font-bold text-lg rounded-lg px-6 py-4 no-underline`}>
+        <Link
+            to={href}
+            className="bg-cyan hover:bg-cyan-dark text-slate-100 font-bold text-lg rounded-lg px-6 py-4 no-underline"
+        >
             {text}
         </Link>
     );
 };
 
-export { PrimaryLinkButton, SecondaryLinkButton };
+export default LinkButton;
--- src/components/index/jumbotron.tsx
+++ src/components/index/jumbotron.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { StaticImage } from "gatsby-plugin-image";
-import { SecondaryLinkButton } from "@/components/common/link-button";
+import LinkButton from "@/components/common/link-button";
 
 const Jumbotron: FC = () => {
     return (
@@ -15,14 +15,14 @@ const Jumbotron: FC = () => {
                 className="grid-area-1-1 opacity-80 brightness-60"
             />
             <div className="grid-area-1-1 text-center relative place-items-center mx-auto px-4 py-24 lg:py-40">
-                <h1 className="font-kg-second-chances mb-4 py-4 md:py-8 tracking-tight leading-none text-gray-100 text-4xl md:text-6xl lg:text-7xl text-shadow">
+                <h1 className="font-kg-second-chances mb-4 py-4 md:py-8 tracking-tight leading-none text-slate-100 text-4xl md:text-6xl lg:text-7xl text-shadow">
                     A safe, welcoming and fun environment
                 </h1>
-                <p className="font-black text-gray-100 text-xl text-shadow md:text-2xl lg:text-4xl mb-8 py-4 md:py-8 sm:px-16 lg:px-48">
+                <p className="font-black text-slate-100 text-xl text-shadow md:text-2xl lg:text-4xl mb-8 py-4 md:py-8 sm:px-16 lg:px-48">
                     Situated in the heart of Copmanthorpe village in York, we aim to provide children with an exciting
                     journey of learning through play
                 </p>
-                <SecondaryLinkButton text="Watch our virtual tour" href="#" />
+                <LinkButton text="Watch our virtual tour" href="#" />
             </div>
         </section>
     );
