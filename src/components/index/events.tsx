6740090bf5eeed17dbd99fb15505490ce6b0a668
Chris Evans <chris@ceva24.dev>
2023-09-28 21:58:43 +0100

    feat: use section tags

---
+++ src/components/index/events.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Events: FC = () => {
+    return (
+        <>
+            <h2 className="font-dk-crayon-crumble mb-12 tracking-tight leading-none text-4xl">Upcoming events</h2>
+        </>
+    );
+};
+
+export default Events;
