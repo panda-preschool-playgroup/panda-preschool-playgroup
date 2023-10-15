0570e45de7d815387f74f1727e480630beeee0d7
Chris Evans <chris@ceva24.dev>
2023-10-15 22:13:10 +0100

    feat: set a max page width

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,24 +3,24 @@ import Navigation from "@/components/layout/header/navigation";
 import FooterContent from "@/components/layout/footer/footer-content";
 import SiteBanner from "@/components/layout/footer/hosting-banner";
 
-const globalStyles = "bg-slate-100 text-slate-800 text-xl font-quicksand w-full";
-
 interface LayoutProps {
     activeSection?: string;
 }
 
 const Layout: FC<PropsWithChildren<LayoutProps>> = ({ activeSection, children }: PropsWithChildren<LayoutProps>) => {
     return (
-        <>
-            <header className={globalStyles}>
-                <Navigation activeSection={activeSection} />
-            </header>
-            <main className={globalStyles}>{children}</main>
-            <footer className={globalStyles}>
-                <FooterContent />
-                <SiteBanner />
-            </footer>
-        </>
+        <div className="flex justify-center bg-slate-100 text-slate-800 text-xl font-quicksand">
+            <div className="max-w-[1920px]">
+                <header>
+                    <Navigation activeSection={activeSection} />
+                </header>
+                <main>{children}</main>
+                <footer>
+                    <FooterContent />
+                    <SiteBanner />
+                </footer>
+            </div>
+        </div>
     );
 };
 
