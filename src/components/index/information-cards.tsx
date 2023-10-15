d90aca31b3bfb74df9f19837dacb8374ce1ce90b
Chris Evans <chris@ceva24.dev>
2023-10-15 21:30:00 +0100

    feat: improve layout on xl screen sizes

--- src/components/index/information-cards.tsx
+++ src/components/index/information-cards.tsx
@@ -4,11 +4,11 @@ import Events from "@/components/index/events";
 
 const InformationCards: FC = () => {
     return (
-        <div className="py-24 xl:px-48 md:px-24 px-12 xl:grid grid-cols-2 gap-24 justify-items-center bg-emerald-700">
-            <section className="rounded-lg w-full text-center p-12 bg-slate-100 mb-12 xl:mb-0">
+        <div className="py-24 2xl:px-48 md:px-24 px-12 2xl:grid grid-cols-2 gap-24 justify-items-center bg-emerald-700">
+            <section className="rounded-lg w-full text-center p-12 bg-slate-100 mb-12 2xl:mb-0">
                 <TermDates />
             </section>
-            <section className="rounded-lg w-full text-center p-12 bg-slate-100 mb-12 xl:mb-0">
+            <section className="rounded-lg w-full text-center p-12 bg-slate-100 mb-12 2xl:mb-0">
                 <Events />
             </section>
         </div>
