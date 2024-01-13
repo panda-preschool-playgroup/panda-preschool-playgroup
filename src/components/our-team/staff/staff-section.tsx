27c7ce868301473c0ae47e08ce49e10c1d965dd5
Chris Evans <chris@ceva24.dev>
2024-01-13 15:32:08 +0000

    feat: add staff page (#156)
    * feat: add our staff page
    
    * feat: retrieve staff details from CMS
    
    * feat: add support for staff profile pictures
    
    * chore: temporarily remove 'our team' nav item

---
+++ src/components/our-team/staff/staff-section.tsx
@@ -0,0 +1,55 @@
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image";
+import { FC, PropsWithChildren } from "react";
+
+interface StaffSectionProps {
+    style: "standard" | "alt";
+    name: string;
+    title: string;
+    image?: Queries.Maybe<Queries.ContentfulAsset>;
+}
+
+const StaffSection: FC<PropsWithChildren<StaffSectionProps>> = ({
+    style,
+    name,
+    title,
+    image,
+    children,
+}: PropsWithChildren<StaffSectionProps>) => {
+    return (
+        <section
+            className={`py-24 2xl:px-64 lg:px-48 md:px-24 px-12 ${
+                style === "alt" ? " bg-emerald-700 text-slate-100 text-right" : ""
+            }`}
+        >
+            <h3 className="font-dk-crayon-crumble mb-3 tracking-tight leading-none text-5xl lg:text-6xl">{name}</h3>
+            <p className="text-2xl">
+                <strong>{title}</strong>
+            </p>
+            <div className="2xl:flex 2xl:gap-36">
+                <div className={`shrink-0 flex justify-center ${style === "standard" ? " 2xl:order-last" : ""}`}>
+                    <div className="md:shrink-0 pb-8 2xl:pb-0">
+                        {image?.gatsbyImageData ? (
+                            <GatsbyImage
+                                image={image.gatsbyImageData}
+                                alt={image.title ?? ""}
+                                aria-hidden
+                                className="rounded"
+                            />
+                        ) : (
+                            <StaticImage
+                                src="../../../images/logo.png"
+                                alt={`Placeholder portrait for ${name}`}
+                                aria-hidden
+                                width={250}
+                                height={250}
+                            />
+                        )}
+                    </div>
+                </div>
+                <div className="self-center">{children}</div>
+            </div>
+        </section>
+    );
+};
+
+export default StaffSection;
---
+++ src/pages/our-team/staff.tsx
@@ -0,0 +1,66 @@
+import { FC } from "react";
+import { graphql } from "gatsby";
+import { renderRichText } from "gatsby-source-contentful/rich-text";
+import Layout from "@/components/layout/layout";
+import PageHead from "@/components/layout/page-head/page-head";
+import Article from "@/components/common/article";
+import StaffSection from "@/components/our-team/staff/staff-section";
+
+const heading = "Staff";
+
+interface StaffPageProps {
+    data: Queries.Query;
+}
+
+const StaffPage: FC<StaffPageProps> = ({ data }: StaffPageProps) => {
+    const personList: Queries.Maybe<Queries.ContentfulPersonList> = data.contentfulPersonList;
+
+    return (
+        <Layout showHomeLink activeSection="Our team" pageHeading={heading}>
+            <Article heading={heading}>
+                <p className="pb-12">
+                    Panda Playgroup has an exemplary team of qualified, dedicated staff, all of whom have received
+                    training in First Aid, Food Hygiene and Safeguarding Children, which they update every three years.
+                    In addition, they carry out annual training to meet statutory requirements for delivering the Early
+                    Years Foundation Stage to a high standard. Our caring and dedicated staff are always approachable if
+                    you need a chat!
+                </p>
+            </Article>
+            {personList?.people?.length &&
+                personList.people.map((person: Queries.Maybe<Queries.ContentfulPerson>, index) => (
+                    <StaffSection
+                        key={person?.id}
+                        style={index % 2 === 0 ? "alt" : "standard"}
+                        name={person?.name ?? ""}
+                        title={person?.title ?? ""}
+                        image={person?.profilePicture}
+                    >
+                        {person?.description && renderRichText(person.description)}
+                    </StaffSection>
+                ))}
+        </Layout>
+    );
+};
+
+export default StaffPage;
+
+export const Head = () => <PageHead pageTitle={heading} />;
+
+export const query = graphql`
+    {
+        contentfulPersonList(name: { eq: "Staff" }) {
+            people {
+                id
+                name
+                title
+                description {
+                    raw
+                }
+                profilePicture {
+                    title
+                    gatsbyImageData(height: 250, width: 250, placeholder: BLURRED)
+                }
+            }
+        }
+    }
+`;
